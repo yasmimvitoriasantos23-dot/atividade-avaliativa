1a10bd4295ca4d08c92ca76eaf90e8fce59dae25
yasmimvitoriasantos23-dot <yasmim.vitoria.santos23@escola.pr.gov.br>
2026-07-02 08:57:30 -0300

    estamos criando um site sobre supernatural

---
+++ script.js
@@ -0,0 +1,8 @@
+const botao = document.querySelector("button")
+botao.addEventListener("click", curtir)
+
+
+function curtir(){
+    let curtidas = document.querySelector("span")
+    curtidas.textContent++;
+}
